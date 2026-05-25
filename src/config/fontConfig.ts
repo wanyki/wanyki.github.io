2989b24d4eff73a534f6a7797fa948c055a2d1d8
wanyki <wanyki@126.com>
2026-05-25 14:03:03 +0800

    一些修改

--- src/config/fontConfig.ts
+++ src/config/fontConfig.ts
@@ -5,7 +5,7 @@ export const fontConfig = {
 	// 是否预加载字体文件
 	preload: true,
 	// 当前选择的字体，支持多个字体组合
-	selected: ["my-font"],
+	selected: ["misans-regular"],
 
 	// 字体列表
 	// 推荐使用可靠的 CDN 服务商提供的字体链接，它天然做了按需分片加载，且性能较好
@@ -49,19 +49,6 @@ export const fontConfig = {
 			weight: 400,
 			display: "swap" as const,
 		},
-
-
-
-		
-		"my-font": {
-    		id: "my-font",
-    		name: "MyFont",
-    		src: "https://cdn.jsdelivr.net/gh/wanyki/MyCDN/font/fz.TTF",
-    		family: "MyFont",
-    		display: "swap",
-		},
-
-
 
 		// 小米字体 - MiSans Regular
 		"misans-regular": {
