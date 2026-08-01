55c8482ce989d8fe9e2f15b3418ff4179085124d
wanyki <wanyki@126.com>
2026-08-01 18:14:47 +0800

    Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,7 +67,7 @@ const adapter = process.env.CF_WORKERS
 export default defineConfig({
 	site: siteConfig.site_url,
 
-	base: "/",
+	base: "/Firefly",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建
